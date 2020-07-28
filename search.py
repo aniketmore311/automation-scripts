a704fd3875a46d4b069b7244447fe554b3837598
aniket more <aniketavinashmore343@gamil.com>
2020-07-28 12:05:01 +0530

    modify search.py

--- search.py
+++ search.py
@@ -3,9 +3,10 @@ import webbrowser
 import sys
 
 # a script to do internet searches from the terminal
+
 # example
 # $ ./search google "search one" "search two"
-
+# this will open 2 tabs with google searches "search one" and "search two"
 
 # steps
 # decide the search engine
@@ -13,32 +14,35 @@ import sys
 #   form a structured query and search in a new tab
 
 
-# dictionary containing the base queries of all the search engines
+# dictionary containing the base queries and joining characters of all the search engines
 baseQueries = {
-    "google": "https://www.google.com/search?q=",
-    "youtube": "https://www.youtube.com/results?search_query="
+    "google": ["https://www.google.com/search?q=", "%20"],
+    "youtube": ["https://www.youtube.com/results?search_query=", "%20"],
+    "cppref": ["https://en.cppreference.com/mwiki/index.php?title=Special%3ASearch&search=", "+"],
+    "stacko": ["https://stackoverflow.com/search?q=", "+"]
 }
 
 
 def makeQueries(baseQuery, joiningChar):
     """
-    Funtion to process queries
+    Funtion to process search queries
 
     Parameters:
-        baseQuery(string): base query specific to the search engine  
+        baseQuery(string): base query specific to the search engine
         joiningChar(string): the string used to join saperate terms in the query
     Returns:
         result(list[(string)]): a list of structured queries
 
     """
-    result = []
-    for query in sys.argv[2:]:  # for every individual query
+    results = []
+    searchQueries = sys.argv[2:]
+    for query in searchQueries:  # for every individual query
         queryList = query.split()  # split individual terms in a query
         # join them back with the joining char between them
         formatedQuery = joiningChar.join(queryList)
         # append the structured query to the result
-        result.append(baseQuery + formatedQuery)
-    return result
+        results.append(baseQuery + formatedQuery)
+    return results
 
 
 def openTabs(allQueries):
@@ -48,13 +52,13 @@ def openTabs(allQueries):
 
 
 def main():
-    baseQuery = ""
     # selecting the correct base querie
     engine = sys.argv[1]
-    baseQuery = baseQueries[engine]
+    baseQuery = baseQueries[engine][0]
+    joiningChar = baseQueries[engine][1]
 
     allQueries = []
-    allQueries = makeQueries(baseQuery, "%20")
+    allQueries = makeQueries(baseQuery, joiningChar)
     # print(allQueries, end="\n")
     openTabs(allQueries)
 
