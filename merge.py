61ea99617ddee9dec9c33e81db59d6745868901d
Aniket More <63508109+aniketmore311@users.noreply.github.com>
2020-11-21 20:59:51 +0530

    update merge.py

--- merge.py
+++ merge.py
@@ -17,7 +17,7 @@ def merge_pdfs(paths, output):
 
 
 def main():
-    if sys.argv[1] == "h" or sys.argv[1] == "--help":
+    if sys.argv[1] == "-h" or sys.argv[1] == "--help":
         print("usage: output_file.pdf input_file1.pdf input_file2.pdf ...\n")
         exit()
     paths = sys.argv[2:]
